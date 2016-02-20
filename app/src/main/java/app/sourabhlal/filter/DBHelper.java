9713beb9d414ba3316a15ef6713e9036badd065d
sofiaelkhamlichi <elkhamlichi.sofia@gmail.com>
2016-02-20 19:18:27 +0100

    add*/get* functions done

--- app/src/main/java/app/sourabhlal/filter/DBHelper.java
+++ app/src/main/java/app/sourabhlal/filter/DBHelper.java
@@ -102,23 +102,99 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public void addLabel(Label label) {
         // TODO: 2/20/2016 Louis/Sofia
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues v =new ContentValues();
+        v.put(col_labelID,label.getName());
+
+        db.insert(Table_Labels, null, v);
+        db.close();
     }
+
 
     // READ(ALL)
     public Contact getContact(int id) {
         // TODO: 2/20/2016 Louis/Sofia
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Table_Contacts, new String[] { col_contactID,
+                        col_Name, col_PhoneNumber }, col_contactID + "=?",
+                new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact(cursor.getString(1));
+        contact.setNumber(cursor.getString(0));
+        contact.setEmail(cursor.getString(1));
+        contact.setAddress(cursor.getString(2));
+        contact.setWebsite(cursor.getString(3));
+        return contact ;
     }
 
     public Label getLabel(int id) {
         // TODO: 2/20/2016 Louis/Sofia
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Table_Labels, new String[] { col_labName,
+                        col_labID }, col_labID + "=?",
+                        new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        Label label = new Label(cursor.getString(1));
+        label.setId(Integer.parseInt(cursor.getString(1)));
+        return label ;
     }
+
 
     public List<Contact> getAllContacts() {
         // TODO: 2/20/2016 Louis/Sofia
+        List<Contact> contactList = new ArrayList<Contact>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + Table_Contacts;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Contact contact = new Contact(cursor.getString(1));
+                contact.setId(Integer.parseInt(cursor.getString(0)));
+                contact.setNumber(cursor.getString(2));
+                contact.setEmail(cursor.getString(3));
+                contact.setAddress(cursor.getString(4));
+                contact.setWebsite(cursor.getString(5));
+                // Adding contact to list
+                contactList.add(contact);
+                } while (cursor.moveToNext());
+            }
+
+            // return contact list
+            return contactList;
     }
 
     public List<Label> getAllLabels() {
         // TODO: 2/20/2016 Louis/Sofia
+        List<Label> labelList = new ArrayList<Label>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + Table_Labels;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Label label = new Label(cursor.getString(1));
+                label.setId(Integer.parseInt(cursor.getString(0)));
+                // Adding label to list
+                labelList.add(label);
+            } while (cursor.moveToNext());
+        }
+
+        // return contact list
+        return labelList;
     }
 
     public List<Contact> getContactsByLabel(Label label) {
@@ -144,6 +220,13 @@ public class DBHelper extends SQLiteOpenHelper {
 
     public int getLabelsCount() {
         // TODO: 2/20/2016 Louis/Sofia
+        String countQuery = "SELECT  * FROM " + Table_Labels;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+
+        // return count
+        return cursor.getCount();
     }
 
     //UPDATE
