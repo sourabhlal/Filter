a0fa57fa27d293afec77ec8c4819a5d4c66600ad
sofiaelkhamlichi <elkhamlichi.sofia@gmail.com>
2016-02-21 02:33:04 +0100

    first commit

--- app/src/main/java/app/sourabhlal/filter/ContactSpec.java
+++ app/src/main/java/app/sourabhlal/filter/ContactSpec.java
@@ -2,15 +2,16 @@ package app.sourabhlal.filter;
 
 import com.yahoo.squidb.annotations.ColumnSpec;
 import com.yahoo.squidb.annotations.TableModelSpec;
+import com.yahoo.squidb.data.TableModel;
 
 /**
  * Created by jpbreuer on 2/21/16.
  */
 
 // This is a table schema
-@TableModelSpec(className = "Contact", tableName = "Contacts")
+@TableModelSpec(className = "Contact", tableName = "contact")
 
-public class ContactSpec {
+public class ContactSpec  {
 
     public String Name;
     public String Number;
--- app/src/main/java/app/sourabhlal/filter/MyDataBase.java
+++ app/src/main/java/app/sourabhlal/filter/MyDataBase.java
@@ -1,7 +1,46 @@
 package app.sourabhlal.filter;
 
+import android.content.Context;
+import app.sourabhlal.filter.Contact;
+import com.yahoo.squidb.data.SquidDatabase;
+import com.yahoo.squidb.data.adapter.SQLiteDatabaseWrapper;
+import com.yahoo.squidb.sql.Table;
+
 /**
  * Created by elkhamlichisofia on 21/02/16.
  */
-public class MyDataBase {
+// This is how you'd set up a database instance
+public class MyDataBase extends SquidDatabase {
+
+    private static final int VERSION = 1;
+
+    public MyDataBase(Context context) {
+        super(context);
+    }
+
+    @Override
+    public String getName() {
+        return "my-database.db";
+    }
+
+    @Override
+    protected Table[] getTables() {
+        return new Table[]{
+                // List all tables here
+                Contact.TABLE;
+        };
+    }
+
+    @Override
+    protected boolean onUpgrade(SQLiteDatabaseWrapper db, int oldVersion, int newVersion) {
+        return false;
+    }
+
+    @Override
+    protected int getVersion() {
+        return VERSION;
+    }
+
+    // Other overridable methods exist for migrations and initialization;
+    // omitted for brevity
 }
