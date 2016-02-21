3b29fb5851b10e49026748e4a78c25edb209358e
jpbreuer <jeanpaul.breuer@gmail.com>
2016-02-21 01:57:05 +0100

    created labelspec class

---
+++ app/src/main/java/app/sourabhlal/filter/LabelSpec.java
@@ -0,0 +1,12 @@
+package app.sourabhlal.filter;
+
+import com.yahoo.squidb.annotations.TableModelSpec;
+
+/**
+ * Created by jpbreuer on 2/21/16.
+ */
+
+@TableModelSpec(className = "Label", tableName = "Labels")
+public class LabelSpec {
+    public String Labels;
+}
