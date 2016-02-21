0a2555c6e619affd9666e772f0bfa96f4ca0e81e
sourabhlal <sourabhlal@yahoo.co.in>
2016-02-21 08:15:07 +0100

    can see a new contact

--- app/src/main/java/app/sourabhlal/filter/ContactActivity.java
+++ app/src/main/java/app/sourabhlal/filter/ContactActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 
@@ -24,8 +25,18 @@ public class ContactActivity extends AppCompatActivity {
         Gson gS = new Gson();
         Contact contact = gS.fromJson(target, Contact.class); // Converts the JSON String to an Object
         Log.d("CONTACT", contact.getName());
+        final TextView name = (TextView) findViewById(R.id.conName);
+        final TextView phone = (TextView) findViewById(R.id.conPhone);
+        final TextView email = (TextView) findViewById(R.id.conEmail);
+        final TextView website = (TextView) findViewById(R.id.conWebsite);
+        final TextView address = (TextView) findViewById(R.id.conAddress);
+        name.setText(contact.getName());
+        phone.setText(contact.getNumber());
+        email.setText(contact.getEmail());
+        website.setText(contact.getWebsite());
+        address.setText(contact.getAddress());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        
+
     }
 
 }
